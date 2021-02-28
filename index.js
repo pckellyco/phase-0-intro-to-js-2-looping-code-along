e17cb638cf98391eb036d55d6f1b568167cb9af6
Paige Kelly <pckelly.co@gmail.com>
2021-02-28 14:32:51 -0700

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`)
+}
+
+const gifts = ['teddy bear', 'drone', 'doll'];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts)
+
+function writeCards(names, holiday) {
+    let thankYouCards = []
+    for (let person = 0; person < names.length; person++) {
+        thankYouCards.push(`Thank you, ${names[person]}, for the wonderful ${holiday} gift!`)
+    }
+
+    return thankYouCards;
+}
+
+writeCards(names, holiday)
+
+function countDown(n) {
+    let currentNum = n
+    while (currentNum >= 0) {
+        console.log(currentNum)
+        currentNum--
+    }
+}
